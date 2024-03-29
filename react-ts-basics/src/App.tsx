b6f0b20b209dfdef50aa741260206b4522950f21
nigma_silmi <nigma.silmi@globant.com>
2024-03-29 18:56:17 -0300

    useState to add items - boilerplate

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,13 +1,37 @@
 import CourseGoal from "./components/CourseGoal";
 import { Header } from "./components/Header";
 import headerImage from "../public/goals.jpg";
+import { useState } from "react";
+
+type Goal = { id: number; title: string; description: string };
+
 export default function App() {
+  const [goals, setGoals] = useState<Goal[]>([]);
+
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: Goal = {
+        id: 1,
+        title: "temporary",
+        description: "some dummy description",
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
       <Header src={headerImage} alt="A list of goals"></Header>
-      <CourseGoal title={"do something"}>
-        <p>some description</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {goals.map((gl) => (
+          <li key={gl.id}>
+            <CourseGoal title={gl.title}>
+              <p>{gl.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
