86219d984bebcc17e89f336bba40d8d9d9a872a1
nigma_silmi <nigma.silmi@globant.com>
2024-03-29 14:23:32 -0300

    alternative to typing a component as function expression

--- react-ts-basics/src/components/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -1,6 +1,6 @@
 // special decorator to indicate to the compiler indicating that it's safe be removed
 // from the build
-import { type PropsWithChildren } from "react";
+import { FC, type PropsWithChildren } from "react";
 
 // interface CourseGoalProps {
 //   title: string;
@@ -8,7 +8,19 @@ import { type PropsWithChildren } from "react";
 // }
 type CourseGoalProps = PropsWithChildren<{ title: string }>;
 
-export default function CourseGoal({ title, children }: CourseGoalProps) {
+// export default function CourseGoal({ title, children }: CourseGoalProps) {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>delete</button>
+//     </article>
+//   );
+// }
+
+const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
   return (
     <article>
       <div>
@@ -18,4 +30,6 @@ export default function CourseGoal({ title, children }: CourseGoalProps) {
       <button>delete</button>
     </article>
   );
-}
+};
+
+export default CourseGoal;
