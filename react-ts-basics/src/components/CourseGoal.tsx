d899f17fb27b434c8716808224630e4693731939
nigma_silmi <nigma.silmi@globant.com>
2024-03-29 14:14:23 -0300

    alternative for type PropsWithChildren

--- react-ts-basics/src/components/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -1,11 +1,12 @@
 // special decorator to indicate to the compiler indicating that it's safe be removed
 // from the build
-import { type ReactNode } from "react";
+import { PropsWithChildren, type ReactNode } from "react";
 
-interface CourseGoalProps {
-  title: string;
-  children: ReactNode;
-}
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
 
 export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
