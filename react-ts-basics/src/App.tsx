c14fc017f7dd60cd3cf42d73a4216ffd4065856c
nigma_silmi <nigma.silmi@globant.com>
2024-03-29 14:12:30 -0300

    adding children props

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -2,7 +2,9 @@ import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <CourseGoal title={"do something"} description={"some description"} />
+      <CourseGoal title={"do something"}>
+        <p>some description</p>
+      </CourseGoal>
     </main>
   );
 }
--- react-ts-basics/src/components/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -1,16 +1,20 @@
+// special decorator to indicate to the compiler indicating that it's safe be removed
+// from the build
+import { type ReactNode } from "react";
+
 interface CourseGoalProps {
   title: string;
-  description: string;
+  children: ReactNode;
 }
 
-export default function CourseGoal({ title, description }: CourseGoalProps) {
+export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
-        <p>{description}</p>
-        <button>delete</button>
+        {children}
       </div>
+      <button>delete</button>
     </article>
   );
 }
