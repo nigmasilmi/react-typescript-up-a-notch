2eb876396c569fec7a998ba1fa02516f9cd12341
nigma_silmi <nigma.silmi@globant.com>
2024-04-01 13:20:25 -0300

    type predicate and merging with custom types

--- react-ts-adv/src/App.tsx
+++ react-ts-adv/src/App.tsx
@@ -6,8 +6,6 @@ function App() {
       <Input label="name" id="input-name" type="text" />
       <Input label="age" id="input-age" type="number" />
       <div className="button-group">
-        {/* implementing the type predicate also
-leaves the issue that an anchor could receive a btn property and viceversa */}
         <Button>Click Me</Button>
         <Button href="http://google.com">Navigate from here</Button>
       </div>
--- react-ts-adv/src/Button.tsx
+++ react-ts-adv/src/Button.tsx
@@ -1,17 +1,12 @@
 import { ComponentPropsWithoutRef } from "react";
 
-type ButtonTypeProps = ComponentPropsWithoutRef<"button">;
+type ButtonTypeProps = ComponentPropsWithoutRef<"button"> & { href?: never };
 
-type AnchorTypeProps = ComponentPropsWithoutRef<"a">;
+type AnchorTypeProps = ComponentPropsWithoutRef<"a"> & { href?: string };
 
 type ButtonProps = AnchorTypeProps | ButtonTypeProps;
 
 export default function Button(props: ButtonProps) {
-  // Creating a type predicate
-  // // create a helper function that checks 'property in...'
-  // // to let TS know that we are validating the property,
-  // // // add a type predicate as return type
-
   function isAnchor(props: ButtonProps): props is AnchorTypeProps {
     return "href" in props;
   }
@@ -19,7 +14,6 @@ export default function Button(props: ButtonProps) {
   if (isAnchor(props)) {
     return <a {...props}></a>;
   }
-  // but this button
-  // Type 'string' is not assignable to type '"button" | "submit" | "reset" | undefined'
+
   return <button {...props}>{props.children}</button>;
 }
