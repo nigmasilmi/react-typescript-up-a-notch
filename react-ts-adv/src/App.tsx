0302f745bfe390ce6b0a3ab40eb3ff4fcee0de30
nigma_silmi <nigma.silmi@globant.com>
2024-04-01 08:37:03 -0300

    new project initial setup

--- react-ts-adv/src/App.tsx
+++ react-ts-adv/src/App.tsx
@@ -1,36 +1,5 @@
-import { Header } from "./components/Header";
-import headerImage from "/goals.jpg";
-import { useState } from "react";
-import CourseGoalList from "./components/CourseGoalList";
-import NewGoal from "./components/NewGoal";
-
-export type Goal = { id: number; title: string; description: string };
-
-export default function App() {
-  const [goals, setGoals] = useState<Goal[]>([]);
-
-  function handleAddGoal(goal: string, description: string) {
-    setGoals((prevGoals) => {
-      const newGoal: Goal = {
-        id: Math.random(),
-        title: goal,
-        description,
-      };
-      return [...prevGoals, newGoal];
-    });
-  }
-
-  function handleDeleteGoal(id: number) {
-    setGoals((prevGoals) => {
-      return prevGoals.filter((gl) => gl.id !== id);
-    });
-  }
-
-  return (
-    <main>
-      <Header src={headerImage} alt="A list of goals"></Header>
-      <NewGoal onAddGoal={handleAddGoal} />
-      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
-    </main>
-  );
+function App() {
+  return <h1>Let's get started!</h1>;
 }
+
+export default App;
