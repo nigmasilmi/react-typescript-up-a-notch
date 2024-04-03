65263f20689b2ae60d08e104c71749dda9d95d8b
nigma_silmi <nigma.silmi@globant.com>
2024-04-03 16:25:44 -0300

    fetching and typing data

--- react-ts-adv/src/App.tsx
+++ react-ts-adv/src/App.tsx
@@ -1,5 +1,62 @@
+import { useEffect, useState } from "react";
+import { get } from "./utils/http";
+import BlogPosts, { BlogPost } from "./components/BlogPosts";
+import postHeaderImg from "./assets/data-fetching.png";
+
+// GET	/posts
+// GET	/posts/1
+// GET	/posts/1/comments
+// GET	/comments?postId=1
+// POST	/posts
+// PUT	/posts/1
+// PATCH	/posts/1
+// DELETE	/posts/1
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [data, setData] = useState<BlogPost[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const dummyUrl = "https://jsonplaceholder.typicode.com/posts/5";
+  useEffect(() => {
+    async function fetchNow() {
+      setLoading(true);
+      try {
+        const data = (await get(dummyUrl)) as RawDataBlogPost[];
+        const processedData: BlogPost[] = data.map((item) => {
+          return { id: item.id, title: item.title, text: item.body };
+        });
+        setData(processedData);
+        setLoading(false);
+      } catch (error) {
+        const parsedError = error as Error;
+        setLoading(false);
+        setError(parsedError.message);
+      }
+    }
+    fetchNow();
+  }, []);
+
+  return (
+    <main>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <article>
+          <img src={postHeaderImg} alt="illustration of a thing" />
+          {error && <p>{`Sorry, there was an error. Please try again`}</p>}
+          <BlogPosts posts={data} />
+        </article>
+      )}
+    </main>
+  );
 }
 
 export default App;
